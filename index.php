5ec8b392fc864f2e3cdee84e577d6af7f8befe7d
KevinVigne <kev.vigne@gmail.com>
2025-09-03 16:39:44 +0200

    cours

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cours PHP</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <h1>Les bases en PHP</h1>
+    <nav class ="navbar">
+        <a href="00-bases.php">Les Bases</a>
+        <a href="01-variables.php">Les Variables</a>
+        <a href="02-constante.php">Les Constantes</a>
+        <a href="03-concatenation.php">La Concaténation</a>
+    </nav>
+    <main class="main">
+        
+    </main>
+    <footer class ="footer">
+        <p>© Copyright , Tous droits réservés 2025</p>
+    </footer>
 </body>
 </html>
